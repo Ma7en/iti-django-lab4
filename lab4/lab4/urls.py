5f950ac8141a7f8e159f4bb31003c181e08a993a
Ma7en <m9ee9m@gmail.com>
2024-08-27 00:40:09 +0300

    edit login

--- lab4/lab4/urls.py
+++ lab4/lab4/urls.py
@@ -19,13 +19,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.views import *
+from django.contrib.auth import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("Account/", include("account.urls")),
     path("Track/", include("track.urls")),
     path("Trainee/", include("trainee.urls")),
-    path("", LoginView.as_view(), name="login"),
-    # path("accounts/profile/", .as_view(), name="login"),
+    path("", views.LoginView.as_view(), name="login"),
+    path("Register", views.LogoutView.as_view(), name="register"),
+    # path("accounts/profile/", views.FormView.as_view(), name="profile"),
+    # path("Logout/", views.LogoutView.as_view(), name="logout"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
